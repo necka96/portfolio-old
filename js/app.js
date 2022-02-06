ada82c640226c91cda3f8d5fb3b1f3bee2ef2024
necka96 <nemanja2014@live.com>
2022-02-06 22:53:58 +0100

    about-complete

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const secBtn = document.querySelectorAll(".section-buttons")
 const secBtns = document.querySelectorAll(".button")
 const body = document.querySelector(".main-content")
 
+
 PageTransition()
 function PageTransition (){
  // activ-class-on-btn
@@ -57,3 +58,126 @@ function type (){
  setTimeout(type, 400)
 }
 type()
+
+let numOne = document.getElementById("num-one")
+let numTwo = document.getElementById("num-two")
+let numThree = document.getElementById("num-three")
+let numFour= document.getElementById("num-four")
+let numFive = document.getElementById("num-five")
+let numSix = document.getElementById("num-six")
+let numSeven = document.getElementById("num-seven")
+let numEight = document.getElementById("num-eight")
+let i = 0
+function increaseWidth (){
+  if(i == 0){
+    i = 1
+  var  elOne = document.getElementById("html")
+  var  elTwo = document.getElementById("css")
+  var  elThree = document.getElementById("js")
+  var  elFour = document.getElementById("react")
+  var  elFive = document.getElementById("node")
+  var  elSix = document.getElementById("java")
+  var  elSeven = document.getElementById("git")
+  var  elEight = document.getElementById("scss")
+  var width = 0
+  let intervalOne = setInterval(Html, 60)
+  let intervalTwo = setInterval(Css, 60)
+  let intervalThree = setInterval(Js, 60)
+  let intervalFour = setInterval(React, 60)
+  let intervalFive = setInterval(NodeJs, 60)
+  let intervalSix = setInterval(Java, 60)
+  let intervalSeven = setInterval(Git, 60)
+  let intervalEight = setInterval(Scss, 60)
+  function Html(){
+    if(width >= 77){
+      clearInterval(intervalOne)
+      i=0
+    }else{
+      width ++
+      elOne.style.width = width + "%";
+      numOne.textContent = width + "%";
+    }
+  }
+
+  function Css(){
+    width --;
+    if(width >= 65){
+      clearInterval(intervalTwo)
+      i = 0
+    }else{
+      width ++;
+      elTwo.style.width = width + "%";
+      numTwo.textContent = width + "%";
+    }
+  }
+  function Js (){
+    width --
+  if(width >= 60){
+    clearInterval(intervalThree)
+    i = 0
+  }else{
+    width ++
+    elThree.style.width = width + "%"
+    numThree.textContent = width + "%"
+    
+  }
+  }
+  function React(){
+    width --;
+    if(width >= 50){
+      clearInterval(intervalFour)
+      i=0
+    }else{
+      width ++
+      elFour.style.width = width + "%"
+      numFour.textContent = width + "%"
+    }
+  }
+  function NodeJs(){
+    width --
+    if(width >= 20){
+      clearInterval(intervalFive)
+      i= 0
+    }else{
+      width ++
+      elFive.style.width = width + "%"
+      numFive.textContent = width + "%";
+    }
+  }
+    function Java(){
+    width --
+    if(width >= 20){
+      clearInterval(intervalSix)
+      i= 0
+    }else{
+      width ++
+      elSix.style.width = width + "%"
+      numSix.textContent = width + "%";
+    }
+  }
+  function Git (){
+    width--
+    if(width >= 45){
+      clearInterval(intervalSeven)
+      i = 0
+    }else{
+      width++
+      elSeven.style.width = width + "%"
+      numSeven.textContent = width + "%";
+    }
+  }
+  function Scss (){
+    width --
+    if(width >= 55){
+      clearInterval(intervalEight)
+      i = 0
+    }else{
+      width ++;
+      elEight.style.width = width + "%"
+      numEight.textContent = width + "%"
+    }
+  }
+  }
+
+}
+increaseWidth()
