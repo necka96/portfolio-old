4ff23e19599cae31f7438fed6be2396216dfea69
necka96 <nemanja2014@live.com>
2022-02-07 20:34:22 +0100

    portfolio

--- js/app.js
+++ js/app.js
@@ -181,3 +181,22 @@ function increaseWidth (){
 
 }
 increaseWidth()
+
+const filterButtons = document.querySelector("#filter-btns").children
+const items = document.querySelector(".gallery-container").children
+
+for(let i=0; i < filterButtons.length; i++){
+  filterButtons[i].addEventListener("click", function(){
+   
+    const target = this.getAttribute("data-target")
+    for(let k = 0; k < items.length; k++){
+      items[k].style.display = "none"
+      if(target == items[k].getAttribute("data-id")){
+        items[k].style.display = "block" 
+      }
+      if(target == "all"){
+        items[k].style.display = "block"
+      }
+    }
+  })
+}
